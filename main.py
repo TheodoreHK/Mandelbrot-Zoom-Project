d5400bda4e87ebfc3bc3677a6f92e601ff747e60
adam-e-f <adamfriedman03@gmail.com>
2024-03-17 14:31:48 -0400

    03/17/2024 2:30 PM

--- main.py
+++ main.py
@@ -5,24 +5,28 @@ root = tk.Tk()
 root.title("Mandelbrot Visualizer")
 root.geometry("560x350")
 
+
 def create_clickbutton():
-        global size
-        size = int(text.get())
-        if size > 512:
-                errorSize()
-        else: 
-            MandelbrotMain.make_image((-0.75, 0), 3, size)
+    global size
+    size = int(text.get())
+    if size > 512:
+        errorSize()
+    else:
+        createImage()
+
 
 def errorSize():
-        sizeerror = tk.Label(root, text= "This size is quite large, this could take a while. Do you still want to continue?")
-        sizeerror.place(x=44, y = 275)
-        sizeerror.configure(background="wheat2")
-        confirm = tk.Button(root, text="Yes", command=createImage)
-        confirm.place(x=250, y = 300)
-        confirm.configure(highlightbackground="wheat2")
+    sizeerror = tk.Label(root, text= "This size is quite large, this could take a while. Do you still want to continue?")
+    sizeerror.place(x=44, y = 275)
+    sizeerror.configure(background="wheat2")
+    confirm = tk.Button(root, text="Yes", command=createImage)
+    confirm.place(x=250, y = 300)
+    confirm.configure(highlightbackground="wheat2")
+
 
 def createImage():
-       MandelbrotMain.make_image((-0.75, 0), 3, size)
+    root.destroy()
+    MandelbrotMain.make_image((-0.75, 0), 3, size)
        
 
 text = tk.Entry(root)
